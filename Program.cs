ab41745894f52c949e3747655430ba4fe70dc98d
= <brijendra.sharma2806@gmail.com>
2026-01-12 23:03:42 +0530

    adding things to basic class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CSharpFundamentals.concepts;
+
 Console.WriteLine("Hello, World!");
+BasicClass basicClass = new BasicClass();
+char bchar = basicClass.returnBasicChar();
+int bnumber = basicClass.returnBasicNumber();
+string bname = basicClass.returnBasicName();
+Console.WriteLine($"Called Useless constructor and got char:{bchar}, name:{bname}, number:{bnumber}");
+
+BasicClass basicClassParameterizedConstructor = new BasicClass("Brijendra",7417402,'X');
+char bcharpc = basicClassParameterizedConstructor.returnBasicChar();
+int bnumberpc = basicClassParameterizedConstructor.returnBasicNumber();
+string bnamepc = basicClassParameterizedConstructor.returnBasicName();
+Console.WriteLine($"Called Parameterized constructor and got char:{bcharpc}, name:{bnamepc}, number:{bnumberpc}");
