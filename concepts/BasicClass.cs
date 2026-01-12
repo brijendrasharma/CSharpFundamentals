8a188332edb53b169825d216f67ac503f0317e8b
= <brijendra.sharma2806@gmail.com>
2026-01-12 14:44:09 +0530

    Adding Basic Class

---
+++ concepts/BasicClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals.concepts
+{
+    internal class BasicClass
+    {
+        public BasicClass()
+        {
+            
+        }
+    }
+}
