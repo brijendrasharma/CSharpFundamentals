faf0160e451a7494113d7663ef713b90d2e99eaf
= <brijendra.sharma2806@gmail.com>
2026-01-15 00:46:44 +0530

    Basic Properties Example

---
+++ concepts/BasicProperties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals.concepts
+{
+    internal class BasicProperties
+    {
+        public int LaptopId { get; set; }
+        public string LaptopModel { get; set; }
+        public string LaptopOS { get; set; }
+        public int SSDCapacity { get; set; }
+        public int RAMCapacity { get; set; }
+        public string Processor { get; set; }
+        public string IsTouchScreen { get; set; }
+        public int ScreenSize { get; set; }
+    }
+}
